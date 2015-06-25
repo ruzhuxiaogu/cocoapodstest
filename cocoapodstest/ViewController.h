387a1c3abc01277b9b27822c5bc35eef2c67a5c0
litingdong <litingdong@litingdongdeMacBook-Pro.local>
2015-06-25 11:45:25 +0800

    Initial Commit

---
+++ cocoapodstest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoapodstest
+//
+//  Created by litingdong on 15/6/25.
+//  Copyright (c) 2015年 litingdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cocoapodstest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cocoapodstest
+//
+//  Created by litingdong on 15/6/25.
+//  Copyright (c) 2015年 litingdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
